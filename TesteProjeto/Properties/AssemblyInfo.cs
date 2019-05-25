9f7dbd1e09f5b5f860f6a864a5ad7b4f0725f5fc
Natalia Miranda <nataliamirandademoura@gmail.com>
2019-05-25 00:52:33 -0300

    projeto com teste sem page object

---
+++ TesteProjeto/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TesteProjeto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TesteProjeto")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e690af68-c6ad-4cd9-8425-fb0927e5d363")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TesteProjeto/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TesteProjeto
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://localhost:51739/Estoque/Cadastro");
+
+            IWebElement campo = driver.FindElement(By.Id("Nome"));
+            IWebElement botao = driver.FindElement(By.Id("buttoncreate"));
+            IWebElement mensagem = driver.FindElement(By.Id("mensagemdesucesso"));
+
+            campo.SendKeys("Varejo");
+            botao.Click();
+
+            bool achou = driver.PageSource.Contains("Estoque cadastrado com sucesso.");
+
+            Assert.IsTrue(achou);
+        }
+
+       
+        
+    }
+}
